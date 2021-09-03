caf289ffc6bcb054e2d12bbc52991376b502e63f
Flavio Almeida <falmeida@c2r8s2.42lisboa.com>
2021-09-03 22:01:00 +0100

    Final Changes minishell

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: falmeida <falmeida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:35:11 by falmeida          #+#    #+#             */
-/*   Updated: 2021/09/03 21:02:52 by falmeida         ###   ########.fr       */
+/*   Updated: 2021/09/03 21:54:51 by falmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,11 @@ void	screening(t_mini *mini)
 			ft_echo(mini->argv);
 		else if (!ft_strncmp(mini->argv[0], "cd", ft_strlen(mini->argv[0])))
 			ft_cd();
-		else if (!ft_strncmp(mini->argv[0], "ls", ft_strlen(mini->argv[0])))
-			ft_ls(mini);
 		else
-		{
-			printf("minishell: %s: command not found\n", mini->argv[0]);
-			return ;
-		}
+			ft_ls(mini);
 	}
 	else
 		return ;
-}
-
-int	find_char(char	*str, char c)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
 }
 
 int main()
