933bcc192bc5692783cb4fa4a4b64ef2589f7df8
Flavio Almeida <falmeida@c2r8s2.42lisboa.com>
2021-09-06 15:11:08 +0100

    First Changes falmeida 06/09/2021

--- src/commands/ls.c
+++ src/commands/ls.c
@@ -6,7 +6,7 @@
 /*   By: falmeida <falmeida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 20:54:06 by falmeida          #+#    #+#             */
-/*   Updated: 2021/09/03 21:59:40 by falmeida         ###   ########.fr       */
+/*   Updated: 2021/09/06 15:05:02 by falmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,25 @@
 void	ft_ls(t_mini *mini)
 {
 	mini->str = NULL;
-
 	char *path = "/bin/";
-	char* argv[] = { mini->argv[0], getwd(mini->str) ,NULL};
 	char *command = mini->argv[0];
 	char *total;
+	char* argv[3];
 
+	argv[0] = mini->argv[0];
+	argv[1] = mini->argv[1];
+	argv[2] = NULL;
+
+	if (!ft_strncmp(mini->argv[0], "ls", ft_strlen(mini->argv[0])))
+		argv[1] = getenv("PWD");
 	total = ft_strjoin(path, command);
-
-	pid_t num;
-
-	num = fork();
-
-	if (num == 0)
-		execve(total, argv, NULL);
+	if (fork() == 0)
+	{
+		if ((execve(total, argv, NULL) == -1))
+			printf("minishell: %s: command not found\n", mini->argv[0]);
+	}
 	else
-		return ;
-
+		wait(NULL);
 }
 
 
