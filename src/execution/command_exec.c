8e6f798e7699933a025100e6bfb3a4e1d790ab91
Filipe Ferreira <fferreir@c1r16s10.42lisboa.com>
2021-12-15 15:57:16 +0000

    Implemented and/or [WIP]

--- src/execution/command_exec.c
+++ src/execution/command_exec.c
@@ -6,22 +6,46 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:51:31 by fferreir          #+#    #+#             */
-/*   Updated: 2021/12/06 17:11:11 by fferreir         ###   ########.fr       */
+/*   Updated: 2021/12/15 15:25:13 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-static void	command_exec2(t_cmd *cmd)
+static void	command_exec2(t_cmd *cmd, int *fd)
 {
 	if (cmd->cmd_flags & 1)
 		printf(" WILDE ");
 	else if (cmd->cmd_flags & 2)
 		printf(" EXITE ");
 	else if (cmd->cmd_flags & 4)
-		printf(" AND ");
+	{
+		if (g_mini.first_cmd)
+		{
+			printf("SIMPLE CMD &&\n");
+			simple_command(cmd);
+		}
+		else if (!g_mini.first_cmd)
+		{
+			printf("END CMD &&\n");
+			end_multi_cmd(cmd, fd);
+		}
+		g_mini.and_or_flag++;
+	}
 	else if (cmd->cmd_flags & 8)
-		printf(" OR ");
+	{
+		if (g_mini.first_cmd)
+		{
+			printf("SIMPLE CMD ||\n");
+			simple_command(cmd);
+		}
+		else if (!g_mini.first_cmd)
+		{
+			printf("END CMD ||\n");
+			end_multi_cmd(cmd, fd);
+		}
+		g_mini.and_or_flag++;
+	}
 }
 
 void	command_exec(t_cmd *cmd)
@@ -45,8 +69,8 @@ void	command_exec(t_cmd *cmd)
 			g_mini.exit_status = end_multi_cmd(cmd, g_mini.fd);
 	}
 	else
-		command_exec2(cmd);
-	if (cmd->cmd_flags & 0x20)
+		command_exec2(cmd, g_mini.fd);
+	if (cmd->cmd_flags & 0x04)
 		g_mini.first_cmd = 1;
 	else
 		g_mini.first_cmd = 0;
