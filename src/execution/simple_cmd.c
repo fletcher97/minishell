86ecb3f123a1d6ac34ff592bea1d10fe258ef7dd
Filipe Ferreira <fferreir@c1r16s10.42lisboa.com>
2021-12-17 16:52:07 +0000

    Function modified to receive Exit status from child processes

--- src/execution/simple_cmd.c
+++ src/execution/simple_cmd.c
@@ -6,7 +6,7 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:48:05 by fferreir          #+#    #+#             */
-/*   Updated: 2021/12/15 15:40:36 by fferreir         ###   ########.fr       */
+/*   Updated: 2021/12/17 16:20:43 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ static int	fd_mng_builtins(t_cmd *cmd, int input, int output)
 	{
 		input = file_input(cmd->in.input, cmd->in.heredoc, cmd->in.in);
 		if (input < 0)
-			return (-1);
+			return (EXIT_FAILURE);
 	}
 	if (cmd->in.out)
 		output = file_output(cmd->in.output, cmd->in.append, cmd->in.out);
 	screening_one(cmd->cmd);
-	return (1);
+	return (EXIT_SUCCESS);
 }
 
 static int	fd_mng_child_process(t_cmd *cmd, int input, int output)
@@ -34,7 +34,7 @@ static int	fd_mng_child_process(t_cmd *cmd, int input, int output)
 		if (input > 0)
 			dup2(input, 0);
 		else
-			return (-1);
+			return (EXIT_FAILURE);
 	}
 	if (cmd->in.out)
 	{
@@ -42,35 +42,40 @@ static int	fd_mng_child_process(t_cmd *cmd, int input, int output)
 		if (output > 0)
 			dup2(output, 1);
 		else
-			return (-1);
+			return (EXIT_FAILURE);
 	}
 	cmd_selector(cmd->cmd);
 	exit_fork();
-	return (1);
+	return (EXIT_FAILURE);
 }
 
+//The Simple Command function manages the command call of a single command call.
+//Also, it manages the input and output file descriptors (FD).
 int	simple_command(t_cmd *cmd)
 {
 	int	pid;
-	int	exit_status;
 	int	input;
 	int	output;
+	int	status;
 
 	if (g_mini.stop)
-		return (g_mini.exit_status);
-	exit_status = 0;
+		return (--g_mini.stop);
 	input = 0;
 	output = 0;
-	printf("CMD LINE S = %s\n", cmd->line);
 	if (!cmd_identifier(cmd->cmd))
 		return (fd_mng_builtins(cmd, input, output));
 	pid = fork();
 	if (pid == 0)
-		return (fd_mng_child_process(cmd, input, output));
-	else
-		exit_status = wait(0);
-	if (exit_status < 0)
-		g_mini.stop = 1;
-	printf("ES = %d for %s\n", exit_status, cmd->line);
-	return (exit_status);
+	{
+		fd_mng_child_process(cmd, input, output);
+		return (EXIT_FAILURE);
+	}
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		printf("waitpid failed");
+		return (EXIT_FAILURE);
+	}
+	if (WIFEXITED(status))
+		g_mini.exit_status = WEXITSTATUS(status);
+	return (EXIT_SUCCESS);
 }
