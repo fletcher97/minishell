606e05ed6bafc45963f81090e73885eecdd80cd1
Filipe Ferreira <fferreir@c1r16s10.42lisboa.com>
2021-12-17 16:49:46 +0000

    Added OR/AND instruncitons, OR control flag and norminetted the file

--- src/execution/command_exec.c
+++ src/execution/command_exec.c
@@ -6,13 +6,13 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:51:31 by fferreir          #+#    #+#             */
-/*   Updated: 2021/12/15 15:25:13 by fferreir         ###   ########.fr       */
+/*   Updated: 2021/12/17 15:38:37 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-static void	command_exec2(t_cmd *cmd, int *fd)
+static void	command_call2(t_cmd *cmd, int *fd)
 {
 	if (cmd->cmd_flags & 1)
 		printf(" WILDE ");
@@ -21,57 +21,57 @@ static void	command_exec2(t_cmd *cmd, int *fd)
 	else if (cmd->cmd_flags & 4)
 	{
 		if (g_mini.first_cmd)
-		{
-			printf("SIMPLE CMD &&\n");
 			simple_command(cmd);
-		}
-		else if (!g_mini.first_cmd)
-		{
-			printf("END CMD &&\n");
+		else
 			end_multi_cmd(cmd, fd);
-		}
-		g_mini.and_or_flag++;
+		if (g_mini.exit_status != 0)
+			g_mini.stop++;
 	}
 	else if (cmd->cmd_flags & 8)
 	{
 		if (g_mini.first_cmd)
-		{
-			printf("SIMPLE CMD ||\n");
 			simple_command(cmd);
-		}
-		else if (!g_mini.first_cmd)
-		{
-			printf("END CMD ||\n");
+		else
 			end_multi_cmd(cmd, fd);
-		}
-		g_mini.and_or_flag++;
+		if (g_mini.exit_status == 0)
+			g_mini.stop++;
 	}
 }
 
-void	command_exec(t_cmd *cmd)
+static void	command_call(t_cmd *cmd)
 {
 	if ((cmd->cmd_flags & 0x40) && g_mini.first_cmd)
-		g_mini.exit_status = first_cmd_pipe(cmd, g_mini.fd);
+		first_cmd_pipe(cmd, g_mini.fd);
 	else if (cmd->cmd_flags & 0x40)
-		g_mini.exit_status = pipe_command(cmd, g_mini.fd);
+		pipe_command(cmd, g_mini.fd);
 	else if ((cmd->cmd_flags & 0x10) && g_mini.first_cmd)
-		g_mini.exit_status = simple_command(cmd);
+		simple_command(cmd);
 	else if ((cmd->cmd_flags & 0x10) && !g_mini.first_cmd)
-		g_mini.exit_status = end_multi_cmd(cmd, g_mini.fd);
+		end_multi_cmd(cmd, g_mini.fd);
 	else if (cmd->cmd_flags & 0x20)
 	{
 		if (g_mini.first_cmd)
 		{
-			g_mini.exit_status = simple_command(cmd);
+			simple_command(cmd);
 			g_mini.first_cmd = 1;
 		}
 		else
-			g_mini.exit_status = end_multi_cmd(cmd, g_mini.fd);
+			end_multi_cmd(cmd, g_mini.fd);
 	}
 	else
-		command_exec2(cmd, g_mini.fd);
+		command_call2(cmd, g_mini.fd);
+}
+
+//The Command_exec function will check the command flags and redirect it to the
+//correct function. Also, it will manage the flag for 'first command' call as
+//well as managing the stop flag for 'OR' commands.
+void	command_exec(t_cmd *cmd)
+{
+	command_call(cmd);
 	if (cmd->cmd_flags & 0x04)
 		g_mini.first_cmd = 1;
 	else
 		g_mini.first_cmd = 0;
+	if ((cmd->cmd_flags & 8) && !cmd->cmd[0])
+		g_mini.stop = 0;
 }
