939f3e2ff96786f46c0465835bbb6d5efbfc29a1
Filipe Ferreira <fferreir@c1r16s10.42lisboa.com>
2021-12-27 17:35:04 +0000

    Modified output check for builtin functions and removed dup2 error msgs

--- src/execution/end_cmd.c
+++ src/execution/end_cmd.c
@@ -6,13 +6,13 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:50:12 by fferreir          #+#    #+#             */
-/*   Updated: 2021/12/17 16:07:56 by fferreir         ###   ########.fr       */
+/*   Updated: 2021/12/27 17:20:28 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-static int	fd_mng_builtins(t_cmd *cmd, int fd[2], int input, int output)
+static int	fd_mng_builtins(t_cmd *cmd, int fd[2], int input)
 {
 	close(fd[1]);
 	close(fd[0]);
@@ -20,19 +20,12 @@ static int	fd_mng_builtins(t_cmd *cmd, int fd[2], int input, int output)
 	{
 		input = file_input(cmd->in.input, cmd->in.heredoc, cmd->in.in);
 		if (input > 0 && dup2(input, 0) < 0)
-		{
-			printf("Error: Bad dup2 on input to 0 end multi cmd function");
-			return (EXIT_FAILURE);
-		}
+			return (error_output('i', 0, ++cmd->in.input->content));
 	}
-	else if (g_mini.saved_fd > 0 && dup2(g_mini.saved_fd, 0) < 0)
-		printf("Error: Bad dup2 on saved_fd to 0 end multi cmd function");
+	else if (g_mini.saved_fd > 0)
+		dup2(g_mini.saved_fd, 0);
 	if (cmd->in.out)
-	{
-		output = file_output(cmd->in.output, cmd->in.append, cmd->in.out);
-		if (output > 0 && dup2(output, 1) < 0)
-			printf("Error: Bad dup2 on output to 0 end multi cmd function");
-	}
+		file_output(cmd->in.output, cmd->in.append, cmd->in.out);
 	screening_one(cmd->cmd);
 	close(g_mini.saved_fd);
 	return (EXIT_SUCCESS);
@@ -46,18 +39,15 @@ static int	fd_mng_child_process(t_cmd *cmd, int fd[2], int input, int output)
 	{
 		input = file_input(cmd->in.input, cmd->in.heredoc, cmd->in.in);
 		if (input > 0 && dup2(input, 0) < 0)
-		{
-			printf("Error: Bad dup2 on inpt to 0 end multi cmd child func");
-			return (EXIT_FAILURE);
-		}
+			return (error_output('i', 0, ++cmd->in.input->content));
 	}
-	else if (g_mini.saved_fd > 0 && dup2(g_mini.saved_fd, 0) < 0)
-		printf("Error: Bad dup2 on s_fd to 0 end multi cmd child func\n");
+	else if (g_mini.saved_fd > 0)
+		dup2(g_mini.saved_fd, 0);
 	if (cmd->in.out)
 	{
 		output = file_output(cmd->in.output, cmd->in.append, cmd->in.out);
-		if (output > 0 && dup2(output, 1) < 0)
-			printf("Error: Bad dup2 on outp to 0 end multi cmd child func");
+		if (output > 0)
+			dup2(output, 1);
 	}
 	cmd_selector(cmd->cmd);
 	exit_fork();
@@ -97,7 +87,7 @@ int	end_multi_cmd(t_cmd *cmd, int fd[2])
 	pipe(fd);
 	pid = fork();
 	if (!cmd_identifier(cmd->cmd))
-		return (fd_mng_builtins(cmd, fd, input, output));
+		return (fd_mng_builtins(cmd, fd, input));
 	if (pid == 0)
 	{
 		fd_mng_child_process(cmd, fd, input, output);
