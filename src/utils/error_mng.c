d26469f14cbe459bd97d9ac7d8c0b79b1ccb483f
Filipe Ferreira <fferreir@c1r16s10.42lisboa.com>
2021-12-27 17:38:11 +0000

    added error type for bad input and modified error return type for builtin error control

--- src/utils/error_mng.c
+++ src/utils/error_mng.c
@@ -6,7 +6,7 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 22:57:22 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/12/21 14:48:56 by fferreir         ###   ########.fr       */
+/*   Updated: 2021/12/27 15:24:58 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 //Error output function is used to output an error msg based on the error type
 //it received.
-void	error_output(char type, int i)
+int	error_output(char type, int i, char *str)
 {
 	if (i < 0)
 		i = 0;
@@ -23,9 +23,20 @@ void	error_output(char type, int i)
 		printf("minishell: command not found: %s\n", g_mini.argv[i]);
 		g_mini.exit_status = 127;
 	}
-	if (type == 's')
+	else if (type == 'd')
+	{
+		printf("minishell: cd: %s: No such file or directory\n", g_mini.argv[i]);
+		g_mini.exit_status = 1;
+	}
+	else if (type == 's')
 	{
 		printf("minishell: special character not defined: %s\n", g_mini.argv[i]);
 		g_mini.exit_status = 33;
 	}
+	else if (type == 'i')
+	{
+		printf("minishell: %s: No such file or directory\n", str);
+		g_mini.exit_status = 1;
+	}
+	return (g_mini.exit_status);
 }
