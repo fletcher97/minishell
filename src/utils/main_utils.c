565c4b72db2673d1b752b3015e73e32ab86d8ab3
SherlockPT <filipepipi@hotmail.com>
2022-01-12 20:52:54 +0000

    Added new function to help close fd list and kill processes, added re_init and moved HSI function to main_util.c file

--- src/utils/main_utils.c
+++ src/utils/main_utils.c
@@ -6,21 +6,45 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 18:09:01 by fferreir          #+#    #+#             */
-/*   Updated: 2021/10/14 16:14:37 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/01/12 18:47:38 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utilities.h"
 
-//The args counter function is to count the number of arguments created by the
-//received by the user input and stored on an array by the split function.
-//This function is not being used right now.
-int	args_counter(void)
+void	hsi(int signal)
+{
+	rl_replace_line("", signal);
+	printf("\nminishell: ");
+}
+
+//The Clean Processes function closes all possible open file descriptors and
+//kill all open processes that were not naturally closed.
+
+void clean_processes(void)
 {
 	int	i;
 
-	i = 0;
-	while (g_mini.argv[i])
-		i++;
-	return (i);
+	i = 2;
+	while (++i < MAX_FD)
+		close(i);
+	i = -1;
+	while(g_mini.pid_lst[++i] > 0)
+	{
+		kill(g_mini.pid_lst[i], SIGTERM);
+		g_mini.pid_lst[i] = -1;
+	}
+}
+
+void	re_init(void)
+{
+	clean_processes();
+	g_mini.fd_in = 0;
+	g_mini.fd_out = 1;
+	g_mini.cmd_counter = 0;
+	g_mini.file_counter = 0;
+	g_mini.hdoc_counter = 0;
+	g_mini.and_flag = 0;
+	g_mini.pid_counter = 0;
+	g_mini.stop = 0;
 }
