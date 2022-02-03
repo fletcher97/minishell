0452b3a3c382f71f97192b86e30cfee18e0e2c8d
SherlockPT <filipepipi@hotmail.com>
2022-02-03 23:52:26 +0000

    removed AND/OR check for STOP variable modification and moved to main so it use Exit Status value

--- src/execution/command_exec.c
+++ src/execution/command_exec.c
@@ -6,46 +6,20 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:51:31 by fferreir          #+#    #+#             */
-/*   Updated: 2022/02/03 00:40:27 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/02/03 22:35:24 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
-/*
-*   Checks for AND and OR flags to modify the STOP and First_cmd variables.
-*    for the next cmd call.
-*/
-static void check_and_or_flag(t_cmd *cmd)
-{
-	if (cmd->cmd_flags & 0x08)
-	{
-		if (g_mini.exit_status == 0)
-			g_mini.stop++;
-	}
-	else if (cmd->cmd_flags & 0x04)
-		g_mini.first_cmd = 1;
-	else
-		g_mini.first_cmd = 0;
-}
 
 /*
 *   Checks for the END (;) flag so it can reset the First_cmd variable;
-*   Also, it modifies the STOP variable in case it detects the AND flag;
+*   Also, it modifies the STOP variable in case it detects the OR flag;
 *   Also, stops the cmd to be executed in case STOP variable is > 0.
 */
-static int	command_call(t_cmd *cmd)
+static int	stop_check(t_cmd *cmd)
 {
-	if (cmd->cmd_flags & 0x20)
-			g_mini.first_cmd = 1;
-	else if (cmd->cmd_flags & 0x04)
-	{
-		if (g_mini.exit_status != 0)
-		{
-			g_mini.stop++;
-			g_mini.and_flag++;
-		}
-	}
-	if (g_mini.stop)
+	if (g_mini.stop > 0)
 		return (g_mini.exit_status);
 	return (execute_cmd(cmd));
 }
@@ -72,19 +46,19 @@ int	command_exec(t_cmd *cmd)
 	if (!cmd->cmd[0])
 	{
 		if ((cmd->cmd_flags & 0x08) && g_mini.exit_status == 0)
-		{
-			g_mini.stop = 1;
-			g_mini.and_flag--;
-		}
+			g_mini.stop--;
 		if ((cmd->cmd_flags & 0x04) && g_mini.exit_status != 0)
-			g_mini.stop = 1;
+			g_mini.stop++;
 		g_mini.first_cmd = 1;
 		return (-2);
 	}
 	if (!cmd_identifier(cmd->cmd) && g_mini.first_cmd &&
-			(cmd->cmd_flags & 0x10))
+			((cmd->cmd_flags & 0x10) || (cmd->cmd_flags & 0x20)))
 		return (bultin_exec(cmd));
-	ret = command_call(cmd);
-	check_and_or_flag(cmd);
+	ret = stop_check(cmd);
+	if (cmd->cmd_flags & 0x40)
+		g_mini.first_cmd = 0;
+	else
+		g_mini.first_cmd = 1;
 	return (ret);
 }
