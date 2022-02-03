8035f77fde5a75a302f4d8abd725b6f697e7963d
SherlockPT <filipepipi@hotmail.com>
2022-02-03 23:50:57 +0000

    Added function to create an array with our internal env and pass it to the execve function

--- src/commands/execve.c
+++ src/commands/execve.c
@@ -6,14 +6,17 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 22:30:26 by mgueifao          #+#    #+#             */
-/*   Updated: 2022/01/12 19:20:21 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/02/03 23:48:35 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands.h"
 
-//The Path Creation function is used to differentiate between built-in program
-//calls such as ls, wc, etc, and executable programs calls with "./" or "a.out")
+/*
+*   The Path Creation function is used to differentiate between built-in program
+*    calls such as ls, wc, etc, and executable programs calls with "./" or
+*    "a.out".
+*/
 static char	*path_creation(char *path, char *cmd)
 {
 	char		*temp;
@@ -36,36 +39,75 @@ static char	*path_creation(char *path, char *cmd)
 	return (new_path);
 }
 
-//The path creation loop function is mainly used to cut down lines on the execve
-//function body. It will loop through all the possible path's found in the env
-//"PATH" ($PATH) and try to run each of them with the inputed command.
-//NOTA IMPORTANTE-> Criar copia do env interno em forma de table e null
-//terminated e pasar para execve!
+/*
+*   Copies our internal dual linked env list to a malloced array of strings.
+*    Each entry of the array is also malloced using ft_strjoin function.
+*/
+static char**	temp_env(char **env)
+{
+	t_dl_list	*head;
+	int			size;
+	int			i;
+
+	head = g_mini.env;
+	size = 0;
+	while (g_mini.env)
+	{
+		g_mini.env = g_mini.env->next;
+		size++;
+		if (!g_mini.env)
+			break ;
+	}
+	g_mini.env = head;
+	env = (char **)malloc(sizeof(char*) * (size + 1));
+	i = -1;
+	while (g_mini.env)
+	{
+		env[++i] = ft_strjoin(g_mini.env->name, g_mini.env->content);
+		g_mini.env = g_mini.env->next;
+		if (!g_mini.env)
+			break ;
+	}
+	env[++i] = NULL;
+	g_mini.env = head;
+	return(env);
+}
+
+/*
+*   The path creation loop function is mainly used to cut down lines on the
+*    execve function body. It will loop through all the possible path's found in
+*    the env "PATH" ($PATH) and try to run each of them with the inputed cmd.
+*/
 static int	path_creation_loop(char **cmds, char **path, char *cmd)
 {
+	char	**env;
 	char	*total;
 	int		i;
 	int		j;
 
 	i = -1;
 	j = -1;
+	env = NULL;
 	while (path[++i])
 	{
 		total = path_creation(path[i], cmd);
-		if ((execve(total, cmds, NULL) != -1))
+		if ((execve(total, cmds, temp_env(env)) != -1))
 			--j;
 		free(total);
 		total = NULL;
 		++j;
 	}
+	free_table(env);
 	if (j == i)
 		return (1);
 	return (127);
 }
 
-//The execve function is used to execute various commands such as ls, wc, grep
-//etc. This commands are not mandatory but will improve the user experience and
-//they will help during the program evaluation.
+/*
+*   The execve function is used to execute various commands such as ls, wc, grep
+*    etc. This commands are not mandatory but will improve the user experience
+*    and they will help during the program evaluation.
+*/
 int	ft_execve(char **argv, int i)
 {
 	char	*path;
