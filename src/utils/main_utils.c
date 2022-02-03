f802183f825950f46ef311d755db689cd3906d9e
SherlockPT <filipepipi@hotmail.com>
2022-02-03 23:58:30 +0000

    Added the check AND/OR flag function

--- src/utils/main_utils.c
+++ src/utils/main_utils.c
@@ -6,7 +6,7 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 18:09:01 by fferreir          #+#    #+#             */
-/*   Updated: 2022/02/02 23:45:11 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/02/03 22:54:17 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ void	hsi(int signal)
 	printf("\nminishell: ");
 }
 
-//The Clean Processes function closes all possible open file descriptors and
-//kill all open processes that were not naturally closed.
-
+/*
+*   The Clean Processes function closes all possible open file descriptors and
+*    kill all open processes that were not naturally closed.
+*/
 void clean_processes(void)
 {
 	int	i;
@@ -36,6 +37,10 @@ void clean_processes(void)
 	}
 }
 
+/*
+*   It will re-initialize all the variables that are needed to be reseted for
+*    the input loop. It will also call the Clean Processes function.
+*/
 void	re_init(void)
 {
 	clean_processes();
@@ -44,8 +49,27 @@ void	re_init(void)
 	g_mini.cmd_counter = 0;
 	g_mini.file_counter = 0;
 	g_mini.hdoc_counter = 0;
-	g_mini.and_flag = 0;
 	g_mini.pid_counter = -1;
 	g_mini.stop = 0;
 	g_mini.first_cmd = 1;
+}
+
+/*
+*   It checks the AND and OR flag in case there is a command so it can call the
+*    tree_loop function and so it can execute the next command. This is necessa-
+*    ry because the commands work inside a while loop that is forked and we
+*    we only have access to the Exit Status of the command after it resolves.
+*   It will also modifies the STOP variable at this stage so that the next
+*    command will not resolve in case the STOP is > 0.
+*/
+void check_and_or_flag(t_cmd *cmd, t_tree *t, int i)
+{
+	if (cmd && (cmd->cmd_flags & 0x04 || cmd->cmd_flags & 0x08))
+	{
+		if ((cmd->cmd_flags & 0x08) && g_mini.exit_status == 0)
+			g_mini.stop++;
+		if ((cmd->cmd_flags & 0x04) && g_mini.exit_status != 0)
+			g_mini.stop++;
+		tree_loop(t, i);
+	}
 }
