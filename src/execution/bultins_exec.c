8c9597f3414df78a71bf540f87224618bff06372
SherlockPT <filipepipi@hotmail.com>
2022-02-07 01:53:56 +0000

    chance input exit to -1 in case of failure

--- src/execution/bultins_exec.c
+++ src/execution/bultins_exec.c
@@ -8,7 +8,7 @@ static int	file_input_instruction(t_cmd *cmd, int input)
 		if (dup2(input, 0) != -1)
 			return (EXIT_SUCCESS);
 	}
-	return (error_output('i', 0, cmd->in.input->content + 1));
+	return (-1);
 }
 
 int	bultin_exec(t_cmd *cmd)
@@ -20,7 +20,7 @@ int	bultin_exec(t_cmd *cmd)
 	if (cmd->in.in)
 	{
 		if (file_input_instruction(cmd, input) < 0)
-			return (EXIT_FAILURE);
+			return (-1);
 	}
 	if (cmd->in.out)
 	{
