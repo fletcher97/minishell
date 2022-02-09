85691e03fb667b238910c13f1bacfc4623f12636
SherlockPT <filipepipi@hotmail.com>
2022-02-09 02:26:14 +0000

    Norminetted

--- src/execution/command_exec.c
+++ src/execution/command_exec.c
@@ -6,7 +6,7 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:51:31 by fferreir          #+#    #+#             */
-/*   Updated: 2022/02/08 23:37:52 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/02/09 01:33:02 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 */
 static int	stop_check(t_cmd *cmd)
 {
-	//printf("CMD  = %s ;; STOP CHECK = %d\n", cmd->cmd[0], g_mini.stop);
 	if (g_mini.stop > 0)
 		return (g_mini.exit_status);
 	return (execute_cmd(cmd));
@@ -32,34 +31,34 @@ static int	stop_check(t_cmd *cmd)
 static void	no_cmd_flagged(t_cmd *cmd)
 {
 	if (cmd->cmd_flags & 0x04)
+	{
+		if (g_mini.stop > 0)
+			g_mini.stop = 5;
+		else
+			g_mini.stop = -3;
+		g_mini.and_flag--;
+	}
+	if (cmd->cmd_flags & 0x08)
+	{
+		if (g_mini.and_flag > 0)
 		{
 			if (g_mini.stop > 0)
-				g_mini.stop = 5;
+				g_mini.stop = -4;
 			else
-				g_mini.stop = -3;
-			g_mini.and_flag--;
-		}
-		if (cmd->cmd_flags & 0x08)
-		{
-			if (g_mini.and_flag > 0)
-			{
-				if (g_mini.stop > 0)
-					g_mini.stop = -4;
-				else
-					g_mini.stop = 6;
-				g_mini.and_flag--;
-			}
-			if (g_mini.stop > 0 && g_mini.or_flag > 0)
 				g_mini.stop = 6;
-			g_mini.or_flag--;
+		g_mini.and_flag--;
 		}
-		g_mini.first_cmd = 1;
+		if (g_mini.stop > 0 && g_mini.or_flag > 0)
+			g_mini.stop = 6;
+		g_mini.or_flag--;
+	}
+	g_mini.first_cmd = 1;
 }
 
 /*
-*   The Command_exec function will check the command flags and redirect it to the
-*   correct function. Also, it will manage the flag for 'first command' call as
-*   well as managing the stop flag for 'OR' commands.
+*   The Command_exec function will check the command flags and redirect it to
+*    the correct function. Also, it will manage the flag for 'first command'
+*    call as well as managing the stop flag for 'OR' commands.
 *   Flags used:
 *   If (cmd_flags & 0x01) at least one argument/cmd has a possible wildcard.
 *   If (cmd_flags & 0x02) at least one argument/cmd needs to expand exit code.
@@ -67,8 +66,8 @@ static void	no_cmd_flagged(t_cmd *cmd)
 *   If (cmd_flags & 0x08) cmd is to be executed before a ||
 *   If (cmd_flags & 0x10) cmd is at the end of a list
 *   If (cmd_flags & 0x20) cmd is to be executed before a ; therefore reseting
-*     logic.
-*   If (cmd_flags & 0x40) input piped to next command
+*    logic.
+*   If (cmd_flags & 0x40) input piped to next command.
 */
 int	command_exec(t_cmd *cmd)
 {
@@ -80,8 +79,8 @@ int	command_exec(t_cmd *cmd)
 		no_cmd_flagged(cmd);
 		return (-2);
 	}
-	if (!cmd_identifier(cmd->cmd) && g_mini.first_cmd &&
-			((cmd->cmd_flags & 0x10) || (cmd->cmd_flags & 0x20)))
+	if (!cmd_identifier(cmd->cmd) && g_mini.first_cmd
+		&& ((cmd->cmd_flags & 0x10) || (cmd->cmd_flags & 0x20)))
 		return (bultin_exec(cmd));
 	ret = stop_check(cmd);
 	if (cmd->cmd_flags & 0x40)
