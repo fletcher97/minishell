c089c6bebfad533acca39c5aba264b20b7270d55
Filipe Ferreira <fferreir@c1r11s5.42lisboa.com>
2022-02-10 07:04:49 +0000

    Changed condition for dup2 is used

--- src/execution/execute_cmd.c
+++ src/execution/execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 01:27:47 by fferreir          #+#    #+#             */
-/*   Updated: 2022/02/09 01:41:13 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/02/10 06:53:50 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 /*
 *   It will check for all the input file requests and set the FD for them.
 */
-static void	file_output_instruction(t_cmd *cmd)
+static void file_output_instruction(t_cmd *cmd)
 {
 	if (cmd->in.out)
 		g_mini.fd_out = file_output(cmd->in.output, cmd->in.append,
-				cmd->in.out);
+									cmd->in.out);
 	if (g_mini.fd_out == 1)
 		g_mini.fd_out = dup(g_mini.tmp_out);
 	if (g_mini.fd_out < 0)
@@ -36,11 +36,11 @@ static void	file_output_instruction(t_cmd *cmd)
 *    it will return 0, which will prevent the command to run and exiting the
 *    command loop.
 */
-static int	file_input_instruction(t_cmd *cmd)
+static int file_input_instruction(t_cmd *cmd)
 {
 	if (cmd->in.in)
 		g_mini.fd_in = file_input(cmd->in.input, cmd->in.heredoc, cmd->in.in);
-	if (g_mini.fd_in < 0)
+	if (cmd->in.in && g_mini.fd_in < 0)
 	{
 		dup2(g_mini.tmp_in, 0);
 		dup2(g_mini.tmp_out, 1);
@@ -57,7 +57,7 @@ static int	file_input_instruction(t_cmd *cmd)
 *   Check pipe will check for the pipe flag and, if so, it will pipe the fd so
 *    it can successuly save and forward the correct FD's.
 */
-void	check_pipe(t_cmd *cmd)
+void check_pipe(t_cmd *cmd)
 {
 	if ((cmd->cmd_flags & 0x40) && !cmd->in.out)
 	{
@@ -78,8 +78,9 @@ void	check_pipe(t_cmd *cmd)
 *    all child process that are running will be close in case of a successful
 *    execute, ie, will kill all ghost processes.
 */
-int	execute_cmd(t_cmd *cmd)
+int execute_cmd(t_cmd *cmd)
 {
+	//printf("cmd = %s\n", cmd->cmd[0]);
 	file_output_instruction(cmd);
 	if (!file_input_instruction(cmd))
 		return (-1);
