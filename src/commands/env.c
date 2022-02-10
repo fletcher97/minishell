37d4b9b7e45840df17bc484130074c55ce1dab7a
Filipe Ferreira <fferreir@c2r6s6.42lisboa.com>
2022-02-10 11:47:37 +0000

    Modified comments

--- src/commands/env.c
+++ src/commands/env.c
@@ -6,13 +6,15 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 22:30:10 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/10/20 15:05:35 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/02/10 11:30:37 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands.h"
 
-//The env function is responsible for printing the internal environmental list.
+/*
+*   The env function is responsible for printing the internal environ list.
+*/
 void	ft_env(t_dl_list *env)
 {
 	ft_lstprint(env, 'a');
