af52d535e8a616ecb3d5b62c61b576dfc6233812
Filipe Ferreira <fferreir@c2r6s6.42lisboa.com>
2022-02-10 11:56:16 +0000

    Removed functions that are no longer needed and addedn new ones

--- src/utils/main_utils.c
+++ src/utils/main_utils.c
@@ -6,7 +6,7 @@
 /*   By: fferreir <fferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 18:09:01 by fferreir          #+#    #+#             */
-/*   Updated: 2022/02/10 06:03:03 by fferreir         ###   ########.fr       */
+/*   Updated: 2022/02/10 10:42:42 by fferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,21 @@
 
 #include "readline.h"
 
+#include "ft_stdlib.h"
+#include "ft_string.h"
+
 #include "minishell.h"
 #include "utilities.h"
 
+/*
+*   Sets up the behaviour for the signal when pressing ctr+c.
+*/
 void	hsi(int signal)
 {
 	rl_replace_line("", signal);
 	printf("\n");
 	rl_on_new_line();
 	rl_redisplay();
-}
-
-/*
-*   The Clean Processes function closes all possible open file descriptors and
-*    kill all open processes that were not naturally closed.
-*/
-void	clean_processes(void)
-{
-	int	i;
-
-	i = 2;
-	while (++i < MAX_FD)
-		close(i);
-	i = -1;
-	while (g_mini.pid_lst[++i] > 0)
-	{
-		kill(g_mini.pid_lst[i], SIGTERM);
-		g_mini.pid_lst[i] = -1;
-	}
 }
 
 /*
@@ -60,9 +47,49 @@ void	re_init(void)
 	g_mini.hdoc_counter = 0;
 	g_mini.pid_counter = -1;
 	g_mini.first_cmd = 1;
-	g_mini.stop = 0;
 	g_mini.skip = 0;
-	g_mini.and_flag = 0;
-	g_mini.or_flag = 0;
-	g_mini.es_flag = 0;
+}
+
+/*
+*   Struct init function is used to initiate the stuct variable and cut down
+*    some lines in the main function body.
+*/
+void	struct_init(char **env)
+{
+	g_mini.head = ft_malloc(sizeof(t_dl_list));
+	g_mini.env = get_env(env);
+	g_mini.exit = 0;
+	g_mini.exit_status = 0;
+	g_mini.skip = 0;
+	g_mini.fd_in = 0;
+	g_mini.fd_out = 1;
+	g_mini.hdoc_counter = 0;
+	g_mini.temp_path = ft_strdup("/tmp/");
+	create_hdoc_and_pid_arrays();
+	g_mini.file_counter = 0;
+	g_mini.cmd_counter = 0;
+	g_mini.pid_counter = -1;
+}
+
+/*
+*   Used mainly to save lines. It will create both temp FDs that are a dup from
+*    from 1 and 0 to be used throught the child loops on the tree_loop function
+*    on the main file.
+*/
+int	dup_init_and_close(char type)
+{
+	if (type == 'i')
+	{
+		g_mini.tmp_in = dup(0);
+		g_mini.tmp_out = dup(1);
+		g_mini.fd_in = dup(g_mini.tmp_in);
+	}
+	if (type == 'c')
+	{
+		dup2(g_mini.tmp_in, 0);
+		dup2(g_mini.tmp_out, 1);
+		close(g_mini.tmp_in);
+		close(g_mini.tmp_out);
+	}
+	return (0);
 }
